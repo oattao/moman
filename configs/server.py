be3eb7860bd9958966a9b5ac6d21c3301584350e
oattao <hoang.duy.tang@gmail.com>
2020-05-20 17:30:59 +0900

    Hided some sensitive info.

--- configs/server.py
+++ configs/server.py
@@ -3,7 +3,7 @@ FOLDER_ERROR = {0: 'valid', 1: 'not enough class', 2: 'subfolder has no image', 
 BASE_MODELS = ['Simple', 'Mobilenet', 'Xception']
 MODEL_PATH = 'trained_models'
 LOCAL_HOST = '127.0.0.1'
-PUBLIC_HOST = '10.10.54.121'
+PUBLIC_HOST = ''
 
 WEB_SERVER_PORT = 8080
 UPLOAD_FOLDER = 'static'
